9b614cb5eea60c7b78544f7c55329cb983f54376
irvanwilly13 <irvanwilly9910@gmail.com>
2024-10-08 10:56:07 +0700

    Initial Commit

---
+++ OrderPastaFoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderPastaFoodApp
+//
+//  Created by Muhammad Farrel Al Fathir on 08/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
